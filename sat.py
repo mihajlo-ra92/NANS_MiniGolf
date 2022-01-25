b1917ad3c5290155df2959697595d3374a531110
Mihajlo <maksimovic.ra92.2019@uns.ac.rs>
2022-01-25 02:38:58 +0100

    created SAT collision detection for polygon to polygon and for circle to polygon

--- sat.py
+++ sat.py
@@ -1,117 +1,110 @@
-import numpy as np
-
-def project_point_on_line(point, line):
-    proj = np.array([0.0, 0.0])
-    proj[0] = ((point[0] * line[0] + point[1] * line[1]) / (line[0]**2 + line[1]**2)) * line[0]
-    proj[1] = ((point[0] * line[0] + point[1] * line[1]) / (line[0]**2 + line[1]**2)) * line[1]
-    return proj
-
-
-# This code performs collision testing of convex 2D polyedra by means
-# of the Hyperplane separation theorem, also known as Separating axis theorem (SAT).
-#
-# For more information visit:
-# https://en.wikipedia.org/wiki/Hyperplane_separation_theorem
-#
-# Copyright (C) 2016, Juan Antonio Aldea Armenteros
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
-
-
-# -*- coding: utf8 -*-
-
 from math import sqrt
+from dis import dis
+import numpy as np
+import sys
+def sat_two_polygons(verticies1, verticies2):
+    for i in range(len(verticies1)):
+        vector_a = verticies1[i]
+        #%len(points1) is for the last iteration so we dont go out of range
+        #and we create an edge from the last and the first point
+        vector_b = verticies1[(i + 1) % len(verticies1)]
+        edge = np.array([0.0, 0.0])
+        edge[0] = vector_b[0] - vector_a[0]
+        edge[1] = vector_b[1] - vector_a[1]
+        axis = np.array([edge[1], -edge[0]])
+        min1, max1 = project_vetricies(verticies1, axis)
+        min2, max2 = project_vetricies(verticies2, axis)
+        if min1 >= max2 or min2 >= max1:
+            return False
 
+    for i in range(len(verticies2)):
+        vector_a = verticies2[i]
+        vector_b = verticies2[(i + 1) % len(verticies2)]
+        edge = np.array([0.0, 0.0])
+        edge[0] = vector_b[0] - vector_a[0]
+        edge[1] = vector_b[1] - vector_a[1]
+        axis = np.array([edge[1], -edge[0]]) 
 
-def normalize(vector):
-    """
-    :return: The vector scaled to a length of 1
-    """
-    norm = sqrt(vector[0] ** 2 + vector[1] ** 2)
-    return vector[0] / norm, vector[1] / norm
-
-
-def dot(vector1, vector2):
-    """
-    :return: The dot (or scalar) product of the two vectors
-    """
-    return vector1[0] * vector2[0] + vector1[1] * vector2[1]
-
-
-def edge_direction(point0, point1):
-    """
-    :return: A vector going from point0 to point1
-    """
-    return point1[0] - point0[0], point1[1] - point0[1]
-
-
-def orthogonal(vector):
-    """
-    :return: A new vector which is orthogonal to the given vector
-    """
-    return vector[1], -vector[0]
-
-
-def vertices_to_edges(vertices):
-    """
-    :return: A list of the edges of the vertices as vectors
-    """
-    return [edge_direction(vertices[i], vertices[(i + 1) % len(vertices)])
-            for i in range(len(vertices))]
-
-
-def project(vertices, axis):
-    """
-    :return: A vector showing how much of the vertices lies along the axis
-    """
-    dots = [dot(vertex, axis) for vertex in vertices]
-    return [min(dots), max(dots)]
-
-
-def overlap(projection1, projection2):
-    """
-    :return: Boolean indicating if the two projections overlap
-    """
-    return min(projection1) <= max(projection2) and \
-           min(projection2) <= max(projection1)
-
-
-def separating_axis_theorem(vertices_a, vertices_b):
-    edges = vertices_to_edges(vertices_a) + vertices_to_edges(vertices_b)
-    axes = [normalize(orthogonal(edge)) for edge in edges]
-
-    for axis in axes:
-        projection_a = project(vertices_a, axis)
-        projection_b = project(vertices_b, axis)
-
-        overlapping = overlap(projection_a, projection_b)
-
-        if not overlapping:
+        min1, max1 = project_vetricies(verticies1, axis)
+        min2, max2 = project_vetricies(verticies2, axis)
+        if min1 >= max2 or min2 >= max1:
             return False
 
     return True
 
+def project_vetricies(vetricies, axis):
+    min = sys.maxsize
+    max = -sys.maxsize
+    for i in range(len(vetricies)):
+        v = vetricies[i]
+        #projection is dot product of vetrices and axis
+        proj = dot(v,axis)
+        if proj < min:
+            min = proj
+        if proj > max:
+            max = proj
+    
+    return min, max
 
-def main():
-    test = np.array([[0, 0], [70, 0], [0, 70]])
-    a_vertices = [(0, 0), (70, 0), (0, 70)]
-    b_vertices = [(70, 70), (150, 70), (70, 150)]
-    c_vertices = [(30, 30), (150, 70), (70, 150)]
+def sat_polygon_circle(circle_center, circle_radius, verticies):
 
-    print(separating_axis_theorem(a_vertices, b_vertices))
-    print(separating_axis_theorem(test, c_vertices))
-    print(separating_axis_theorem(b_vertices, c_vertices))
+    normal = np.array([0.0, 0.0])
+    depth = sys.maxsize
+    for i in range(len(verticies)):
+        vector_a = verticies[i]
+        #%len(points1) is for the last iteration so we dont go out of range
+        #and we create an edge from the last and the first point
+        vector_b = verticies[(i + 1) % len(verticies)]
+        edge = np.array([0.0, 0.0])
+        edge[0] = vector_b[0] - vector_a[0]
+        edge[1] = vector_b[1] - vector_a[1]
+        axis = np.array([edge[1], -edge[0]])
+        min1, max1 = project_vetricies(verticies, axis)
+        min2, max2 = project_circle(circle_center, circle_radius, axis)
+        if min1 >= max2 or min2 >= max1:
+            return False
 
 
-if __name__ == "__main__":
-    main()
+    closest_point = find_closest_point_of_polygon(circle_center, verticies)
+    axis = verticies[closest_point] - circle_center
+    min1, max1 = project_vetricies(verticies, axis)
+    min2, max2 = project_circle(circle_center, circle_radius, axis)
+    if min1 >= max2 or min2 >= max1:
+        return False
+
+    return True
+
+def project_circle(circle_center, circle_radius, axis):
+
+    norm = sqrt(axis[0] ** 2 + axis[1] ** 2)
+    axis_normalised = np.array([0.0, 0.0])
+    axis_normalised[0] = axis[0] / norm
+    axis_normalised[1] = axis[1] / norm
+    #we used the normalised axis multiplied by the radius to find the "edges" of the circle
+    point1 = circle_center - axis_normalised * circle_radius
+    point2 = circle_center + axis_normalised * circle_radius
+    #we use dot products to find min and max value
+    min = dot(point1, axis)
+    max = dot(point2, axis)
+    #just in case, if min is greater than the max, swap them
+    if min > max:
+        temp = min
+        min = max
+        max = temp
+
+    return min, max
+
+def dot(value1, value2):
+    return value1[0] * value2[0] + value1[1] * value2[1]
+
+def find_closest_point_of_polygon(point, vetricies):
+    result = -1
+    min_distance = sys.maxsize
+    for i in range(len(vetricies)):
+        current_vetricies = vetricies[i]
+        distance = 0.0
+        distance = sqrt((point[0] - current_vetricies[0])**2 + (point[1] - current_vetricies[1])**2)
+        if distance < min_distance:
+            min_distance = distance
+            result = i
+        return result
--- testing.py
+++ testing.py
@@ -1,53 +1,6 @@
-import square
 import numpy as np
-import sat
-import pygame
-import config
-import game
-square1 = square.Square(np.array([100, 100]))
-square2 = square.Square(np.array([150, 100]))
 
+a = np.array([1, 2])
+b = np.array([3, 4])
 
-game = game.Game()
-
-running = True
-while running:
-    game.screen.fill(config.black)
-    game.draw_squares()
-    
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            #reading the cordinates of where the mouse is pressed
-            game.read_mouse_down(pygame.mouse.get_pos())
-            game.squares[0].center[0] = pygame.mouse.get_pos()[0]
-            game.squares[1].center[1] = pygame.mouse.get_pos()[1]
-            game.draw_squares()
-            # square1.center[0] = pygame.mouse.get_pos()[0]
-            # square1.center[1] = pygame.mouse.get_pos()[1]
-
-        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 3:#3 znaci desni klik
-            print("desni klik")
-            game.read_mouse_down(pygame.mouse.get_pos())
-            game.main_ball.pos[0] = game.mouse_down[0]
-            game.main_ball.pos[1] = game.mouse_down[1]
-            game.main_ball.velocity[0] = 0.0
-            game.main_ball.velocity[1] = 0.0
-
-        if event.type == pygame.MOUSEBUTTONUP:  
-            #reading the cordinates of where the mouse is unpressed
-            game.read_mouse_up(pygame.mouse.get_pos())
-            #if the main ball was clicked, the force that acts on the ball is 
-            #equal to the vector made form the point where the mouse was clicked
-            #and the point where the mouse was unclicked
-            if game.main_ball_clicked:
-                print("ball unclicked")
-                game.calculate_main_ball_force()
-                print(f'force vector is: {game.main_ball_force}')
-                game.main_ball_clicked = False
-                game.main_ball.apply_force(game.main_ball_force)
-            print(game.mouse_up)
-
-    pygame.display.update()
+print(a + b)
